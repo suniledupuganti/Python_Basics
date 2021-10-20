0420500f4a8ad57e7a206dae8c66c633f7ad28ae
sunil <sunil.edupuganti29@gmail.com>
2021-10-20 12:12:53 +0530

    Basics of Python: zfill, index, count, split, replace

--- module1/main.py
+++ module1/main.py
@@ -1,9 +1,21 @@
-# Hello I am a Comment
-print("Hello")
+# zfill function will fill the zeros till it has number
+account_num = '10'
+print(account_num.zfill(16))
 
-var1 = 20
-var2 = 30
-print(var1+var2)
+name = "Sunil is Super"
+print(name.find('u'))
+print(name.index('u'))
+# Difference between find and index is:
+# When we find a character which is not present find will give the output as -1 but index will throw the error
 
-c = 2 & 4
-print(c)
+# Counts the specific character in a String
+print(name.count('S'))
+
+print(name.replace('Super', 'great'))
+
+# Split converts the string to ARRAY
+print(name.split())
+
+if "is" in name:
+    print("Hello")
+
